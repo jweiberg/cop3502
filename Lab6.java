c31adec14d6150fe7530f343a712707de0e6b02f
Dexter <dexter@mantis.gg>
2022-03-14 06:58:20 -0400

    Modified to subtract integers rather than add them.

--- Lab6.java
+++ Lab6.java
@@ -6,13 +6,13 @@ public class Lab6 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("This program adds two integers together!\n");
+        System.out.println("This program subtracts two integers!\n");
         System.out.print("Please enter the first integer: ");
         int input1 = scan.nextInt();
 
         System.out.print("Please enter the second integer: ");
         int input2 = scan.nextInt();
 
-        System.out.println(input1 + " + " + input2 + " is equal to: " + (input1 + input2));
+        System.out.println(input1 + " - " + input2 + " is equal to: " + (input1 - input2));
     }
 }
